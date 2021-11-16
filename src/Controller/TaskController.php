5135ff50cf1e53b990038b621b28a21ccc920983
Flavie Feray <flavie.feray@gmail.com>
2021-11-16 13:24:54 +0100

    creation method et template update task

--- src/Controller/TaskController.php
+++ src/Controller/TaskController.php
@@ -12,7 +12,7 @@ use Symfony\Component\HttpFoundation\Request;
 class TaskController extends AbstractController
 {
     /**
-     * @Route("/task/listing", name="task")
+     * @Route("/task/listing", name="task_listing")
      */
     public function index(): Response
     {
@@ -33,11 +33,56 @@ class TaskController extends AbstractController
     /**
      * @Route("/task/create", name="task_create")
      */
-    public function createTask(Request $request)
+    public function createTask(Request $request): Response
     {
         $task = new Task;
+        $task->setCreatedAt(new \DateTime());
+
         $form = $this->createForm(TaskType::class, $task, []);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            /* $task->setName($form['name']->getDaTa())
+                ->setDescription($form['description']->getDaTa())
+                ->setDueAt($form['dueAt']->getDaTa())
+                ->setTag($form['tag']->getDaTa()); */
+
+            $manager = $this->getDoctrine()->getManager();
+            $manager->persist($task);
+            $manager->flush();
+
+            return $this->redirectToRoute("task_listing");
+        }
+
         return $this->render('task/create.html.twig', [
+            'form' => $form->createView()
+        ]);
+    }
+
+    /**
+     * @Route("/task/update/{id}", name="task_update", requirements={"id"="\d+"})
+     */
+    public function updateTask(int $id, Request $request): Response
+    {
+        $manager = $this->getDoctrine()->getManager();
+        $task = $manager->getRepository(Task::class)->find($id);
+        //$task = $manager->getRepository(Task::class)->findOneBy(['id']);
+        $form = $this->createForm(TaskType::class, $task, []);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            /* $task->setName($form['name']->getDaTa())
+                ->setDescription($form['description']->getDaTa())
+                ->setDueAt($form['dueAt']->getDaTa())
+                ->setTag($form['tag']->getDaTa()); */
+
+
+            $manager->flush();
+
+            return $this->redirectToRoute("task_listing");
+        }
+
+        return $this->render('task/update.html.twig', [
             'form' => $form->createView()
         ]);
     }
