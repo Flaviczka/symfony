1a61a906bfbfc33e2ace42dcb5153efe0df00993
Flavie Feray <flavie.feray@gmail.com>
2021-11-16 10:00:39 +0100

    modification objets task et ajout objets tag + aff

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -2,6 +2,7 @@
 
 namespace App\DataFixtures;
 
+use App\Entity\Tag;
 use App\Entity\Task;
 use Faker\Factory;
 use Doctrine\Persistence\ObjectManager;
@@ -14,6 +15,23 @@ class AppFixtures extends Fixture
         // créer un objet faker
         $faker = Factory::create('fr_FR');
 
+        //creation de nos 5 catégories
+        for ($c = 0; $c < 5; $c++) {
+            //creation d'un nouvel objet Tag
+            $tag = new Tag;
+
+            //nourrir l'objet
+            $tag->setName($faker->colorName());
+
+            $manager->persist($tag);
+        }
+
+        //on push les categorie en BDD
+        $manager->flush();
+
+        //récupération des catégories créées
+        $tags = $manager->getRepository(Tag::class)->findAll();
+
         //création entre 15 et 30 tâches aléatoire
         for ($t = 0; $t < mt_rand(15, 30); $t++) {
             //nouvel objet task
@@ -23,7 +41,8 @@ class AppFixtures extends Fixture
             $task->setName($faker->sentence(6))
                 ->setDescription($faker->paragraph(3))
                 ->setCreatedAt(new \DateTime())
-                ->setDueAt($faker->dateTimeBetween('now', '6 months'));
+                ->setDueAt($faker->dateTimeBetween('now', '6 months'))
+                ->setTag($faker->randomElement($tags));
 
             //faire persister les données
             $manager->persist($task);
